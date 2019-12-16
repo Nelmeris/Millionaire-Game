9aedff6dd5014cffd93d850826605f44bf35152e
Nelmeris <kufaevartem@icloud.com>
2019-12-16 20:17:32 +0300

    Initial Commit

---
+++ Millionaire Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Millionaire Game
+//
+//  Created by Artem Kufaev on 16.12.2019.
+//  Copyright © 2019 Artem Kufaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
